100c2b809a7596bf36f30f809c8966b24ae3c11b
Marvin Lemos <marvinlemos@gmail.com>
2021-01-08 06:27:49 -0300

    minor fixes

--- utils/getUser.js
+++ utils/getUser.js
@@ -1,11 +1,16 @@
-const getUser = async(username) => {
+const getUser = async (username) => {
     const resUser = await fetch(`https://api.github.com/users/${username}`)
     const user = await resUser.json()
 
     const resRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated`)
     const originalRepos = await resRepos.json()
 
-    const dontShowRepos = ['marvinlemos/idswsn', 'marvinlemos/kaggle-google-revenue', 'marvinlemos/supervisely']
+    const dontShowRepos = [
+        'marvinlemos/idswsn',
+        'marvinlemos.dev-api',
+        'marvinlemos/kaggle-google-revenue',
+        'marvinlemos/supervisely'
+    ]
 
     const dontShowFilter = repo => dontShowRepos.indexOf(repo.full_name) === -1
     const isNotForkFilter = repo => !repo.fork
@@ -23,7 +28,7 @@ const getUser = async(username) => {
         .filter(isNotForkFilter)
         .filter(dontShowFilter)
         .map(extractData)
-    
+
     return {
         repos,
         user
